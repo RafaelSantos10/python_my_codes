660f1d4864e7e860de353a65590edcec1f03bf5a
Rafael Santos <raffasatos10@gmail.com>
2023-07-31 10:07:51 -0300

    test home

--- recipes/tests/test_viws_recipes.py
+++ recipes/tests/test_viws_recipes.py
@@ -15,3 +15,18 @@ class RecipeViwsTest(TestCase):
     def test_recipe_datail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 3}))
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            '<h1>No recipes found here</h1>',
+            response.content.decode('utf-8')
+        )
